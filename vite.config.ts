7ecbdb8e55559383b91bd2d5048473cbc3adcb70
Alex B <alexb@Alexs-MacBook-Air.local>
2026-03-07 21:39:16 -0600

    removing build to the repo

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/adhd-daily-planner/', // add this to the base url of the project
 });
